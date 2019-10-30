5c56dec9324d7e45bdfc527b4e35c74fe155f9ad
DIO-MC <25minchul@naver.com>
2019-10-30 20:04:50 +0900

    1st commit

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/minchulkim/Downloads/19FW_REST_PROJECT-master/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
